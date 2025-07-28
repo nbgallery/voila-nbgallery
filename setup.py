196729e6cf74f54ccf7d4a9b2aa5c0ec0e03d995
mild-bit <rromer5@uwe.nsa.gov>
2025-07-28 18:20:45 +0000

    bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ for root, dirs, files in os.walk('share'):
 
 setup(
     name="voila-nbgallery",
-    version="0.0.5.dev0",
+    version="0.0.5",
     description="Voila templates for nbgallery project",
     long_description="Voila templates for nbgallery project",
     long_description_content_type="text/markdown",
